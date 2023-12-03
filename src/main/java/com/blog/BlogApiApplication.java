e76d62aeb47d3530424298595bfb44065a2a3aa0
Hiep <winwaywa@gmail.com>
2023-12-03 10:14:36 +0700

    using ModelMapper

--- src/main/java/com/blog/BlogApiApplication.java
+++ src/main/java/com/blog/BlogApiApplication.java
@@ -1,7 +1,9 @@
 package com.blog;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BlogApiApplication {
@@ -9,5 +11,9 @@ public class BlogApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApiApplication.class, args);
 	}
-
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
