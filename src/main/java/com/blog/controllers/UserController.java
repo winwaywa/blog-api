45dbb8ed0d4b7c7eafa5b3c8a16ae10869a82186
Hiep <winwaywa@gmail.com>
2023-12-03 11:40:32 +0700

    validating using bean validator

--- src/main/java/com/blog/controllers/UserController.java
+++ src/main/java/com/blog/controllers/UserController.java
@@ -18,6 +18,8 @@ import com.blog.payloads.ApiResponse;
 import com.blog.payloads.UserDto;
 import com.blog.services.UserService;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -26,13 +28,13 @@ public class UserController {
 	private UserService userService;
 
 	@PostMapping("/")
-	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+	public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto) {
 		UserDto createdUser = this.userService.createUser(userDto);
 		return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
 	}
 
 	@PutMapping("/{userId}")
-	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable("userId") Integer uid) {
+	public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UserDto userDto, @PathVariable("userId") Integer uid) {
 		UserDto updatedUser = this.userService.updateUser(userDto, uid);
 		return ResponseEntity.ok(updatedUser);
 	}
--- src/main/java/com/blog/payloads/UserDto.java
+++ src/main/java/com/blog/payloads/UserDto.java
@@ -1,5 +1,8 @@
 package com.blog.payloads;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,8 +12,18 @@ import lombok.Setter;
 @Setter
 public class UserDto {
 	private int id;
+
+	@NotEmpty
+	@Size(min = 3, max = 20, message = "Username must be min of 3 chars and max of 20 chars")
 	private String name;
+
+	@Email
 	private String email;
+
+	@NotEmpty
+	@Size(min = 6, max = 20, message = "Username must be min of 6 chars and max of 20 chars")
 	private String password;
+
+	@NotEmpty
 	private String about;
 }
