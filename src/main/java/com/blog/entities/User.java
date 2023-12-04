cae3d8589970e01cedc844506332d5f1da0be026
Hiep <winwaywa@gmail.com>
2023-12-04 21:29:32 +0700

    add relationship one to many

--- src/main/java/com/blog/entities/User.java
+++ src/main/java/com/blog/entities/User.java
@@ -1,10 +1,16 @@
 package com.blog.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,7 +26,7 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
-	
+
 	@Column(name = "user_name", nullable = false, length = 100)
 	private String name;
 
@@ -29,4 +35,7 @@ public class User {
 	private String password;
 
 	private String about;
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Post> posts = new ArrayList<>();
 }
