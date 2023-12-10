a572ba227437a4e31b19aecd6ab0e565c96106bd
Hiep <winwaywa@gmail.com>
2023-12-10 23:07:09 +0700

    add comment api

--- src/main/java/com/blog/entities/Post.java
+++ src/main/java/com/blog/entities/Post.java
@@ -1,7 +1,10 @@
 package com.blog.entities;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,6 +12,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -41,5 +45,8 @@ public class Post {
 
 	@ManyToOne
 	private User user;
+
+	@OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+	private Set<Comment> comments = new HashSet<>();
 
 }
--- src/main/java/com/blog/payloads/PostDto.java
+++ src/main/java/com/blog/payloads/PostDto.java
@@ -1,6 +1,8 @@
 package com.blog.payloads;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,5 +26,7 @@ public class PostDto {
 	private CategoryDto category;
 
 	private UserDto user;
+
+	private Set<CommentDto> comments = new HashSet<>();
 
 }
